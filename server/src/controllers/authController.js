fab606935ae2c5369931a72523b75718824ce101
Triaaz <alimtiaaz23@gmail.com>
2026-07-02 19:58:51 +0100

    Implement auth registration and login services

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -16,6 +16,37 @@ const register = async (req, res) => {
     }
 };
 
+const login = async (data) => {
+    const { email, password } = data;
+
+    // Find user
+    const user = await User.findOne({ email }).populate("business");
+
+    if (!user) {
+        throw new Error("Invalid email or password");
+    }
+
+    // Compare password
+    const isPasswordCorrect = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!isPasswordCorrect) {
+        throw new Error("Invalid email or password");
+    }
+
+    // Remove password before returning
+    const userObject = user.toObject();
+    delete userObject.password;
+
+    return {
+        message: "Login successful",
+        user: userObject,
+    };
+};
+
 module.exports = {
-    register
+    register,
+    login
 };
