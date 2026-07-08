9d3074637e6b35501ede20f8806ba67039e46028
Triaaz <alimtiaaz23@gmail.com>
2026-07-08 09:18:43 +0100

    design Product schema and add product indexes

---
+++ server/src/services/productService.js
@@ -0,0 +1,50 @@
+const product = await ProductService.createProduct(req.user, req.body); 
+const Product = require("../models/product");
+
+const createProduct = async(user, productData) => {
+    const {
+    name,
+    description,
+    sku,
+    barcode,
+    category,
+    supplier,
+    brand,
+    costPrice,
+    sellingPrice,
+    currentStock,
+    reorderLevel,
+    unit,
+    images,
+    attributes
+    } = productData;
+
+    // Check if product with the same SKU already exists
+    const existingSku = awaitProduct.findOne({
+        business:user.business,
+        sku
+    });
+
+    if(existingSku) {
+        throw new Error("Product SKU already exists");;
+    }
+
+    if (barcode) {
+        const existingBarcode = await Product.findOne({
+            business: user.business,
+            barcode
+        });
+
+        if (existingBarcode) {
+            throw new Error("Product barcode already exists.");
+        }
+    }
+}
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProductById,
+    updateProduct,
+    archiveProduct
+};
